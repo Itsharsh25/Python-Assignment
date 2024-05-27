79eb1869797162d38b8ef0b6ef5645506fe6737e
Itsharsh25 <141488411+Itsharsh25@users.noreply.github.com>
2024-05-27 13:20:00 +0530

    Add files via upload

---
+++ assginment1.py
@@ -0,0 +1,32 @@
+# How do you concatenate two strings in Python?
+
+s1 = 'hello my name is'
+s2 = ' harsh'
+c = s1 + s2 
+print(c) 
+
+# What method would you use to convert a string to lowercase in python?
+ 
+s = 'HELLO MY NAME IS HARSH'
+print(s.lower())
+
+# How can you find the length of a string in Python?
+
+s = 'this is the string'
+print(len(s))
+
+# Remove the white space from left side of given string
+st = " hello LIET "
+print(st.lstrip())
+
+# What is the difference between the "insert()" and "append()" methods when adding elements to a list in Python, give an example by coding.
+
+list = [1, 2, 3]
+list.append(4)
+print("After append:", list)
+list.insert(1, 'a')
+print("After insert:",list) 
+
+
+
+
